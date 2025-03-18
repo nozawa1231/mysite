58c09a078ccfc6d1209025a61054d14622973f0e
yuuta4294 <yuuta4294@gmail.com>
2025-03-18 17:59:35 +0900

    add app.py

---
+++ pages/page3.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+st.title("Irisデータを用いた予測アプリ")
+
+iris = load_iris()
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = pd.Series(iris.target, name="species")
+
+st.write(iris)
+st.write(X)
+st.write(y)
